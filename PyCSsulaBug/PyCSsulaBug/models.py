7655233b2fe6e950381b4114a53320ad13818724
marco <devnull@localhost>
2012-06-17 02:14:10 +0800

    pyside: 實作 model

--- PyCSsulaBug/PyCSsulaBug/models.py
+++ PyCSsulaBug/PyCSsulaBug/models.py
@@ -3,9 +3,29 @@
 from PySide import QtCore, QtGui
 import handlers
 
-"""
+
 class SiteModel(QtCore.QAbstractListModel):
 
     def __init__(self, parent=None):
         super(SiteModel, self).__init__(parent)
-"""
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        pass
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        return data
+
+    def headerData(section, orientation, role=Qt.DisplayRole):
+        headerList = ["name", "site", "type", "author", "lastUpdated"]
+        return headerList[section]
+              
+    def flags(self, index):
+        pass
+
+    def setData(self, index, value, role=EditRole):
+        pass
+
+    def appendRow(self):
+        pass
