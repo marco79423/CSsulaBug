adf03102bd144022ace3e8a069107d46455ccdf2
marco <devnull@localhost>
2012-06-18 00:33:49 +0800

    pyside: 完成 model…差強人意

--- PyCSsulaBug/PyCSsulaBug/models.py
+++ PyCSsulaBug/PyCSsulaBug/models.py
@@ -4,28 +4,65 @@ from PySide import QtCore, QtGui
 import handlers
 
 
-class SiteModel(QtCore.QAbstractListModel):
+class SiteModel(QtCore.QObject):
 
     def __init__(self, parent=None):
         super(SiteModel, self).__init__(parent)
+        self._model = QtGui.QStandardItemModel(self)
+        self._proxyModel = QtGui.QSortFilterProxyModel(self)
+        self._siteHandler = handlers.SiteHandler(self)
 
-    def rowCount(self, parent=QtCore.QModelIndex()):
-        pass
+        self._model.setColumnCount(5);
+        self._model.setHorizontalHeaderItem(0, QtGui.QStandardItem("Name"));
+        self._model.setHorizontalHeaderItem(1, QtGui.QStandardItem("Site"));
+        self._model.setHorizontalHeaderItem(2, QtGui.QStandardItem("Type"));
+        self._model.setHorizontalHeaderItem(3, QtGui.QStandardItem("Author"));
+        self._model.setHorizontalHeaderItem(4, QtGui.QStandardItem("LastUpdated"));
 
-    def data(self, index, role):
-        if not index.isValid():
-            return None
-        return data
+        #Filter
+        self._proxyModel.setSourceModel(self._model);
+        self._proxyModel.setDynamicSortFilter(True);
 
-    def headerData(section, orientation, role=Qt.DisplayRole):
-        headerList = ["name", "site", "type", "author", "lastUpdated"]
-        return headerList[section]
-              
-    def flags(self, index):
-        pass
+        #SiteHandler
+        self._siteHandler.setUpdateHandler(handlers.SFUpdateHandler())
+        self._siteHandler.setDownloadHandler(handlers.SFDownloadHandler())
+        
+        self._setConnection()
+        
+    @QtCore.Slot()
+    def update(self):
+        self._siteHandler.update()
 
-    def setData(self, index, value, role=EditRole):
-        pass
+    def model(self):
+        return self._proxyModel
 
-    def appendRow(self):
-        pass
+    def _setConnection(self):
+        self._siteHandler.updateInfo.connect(self._insertOneEntry)
+ 
+    @QtCore.Slot(dict)
+    def _insertOneEntry(self, info):
+        entry = []
+        entry.append(QtGui.QStandardItem(info["name"]))
+        entry.append(QtGui.QStandardItem(info["site"]))
+        entry.append(QtGui.QStandardItem(info["type"]))
+        entry.append(QtGui.QStandardItem(info["author"]))
+        entry.append(QtGui.QStandardItem(info["lastUpdated"]))
+        self._model.appendRow(entry)
+    
+
+if __name__ == "__main__":
+    from PySide import QtGui
+    import sys
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QTableView()
+    m = SiteModel()
+    w.setModel(m)
+    w.show()
+    #m._insertOneEntry(dict(name="a", site="s", type="t", author="a", lastUpdated="l"))
+    #m._insertOneEntry(dict(name="b", site="s", type="t", author="a", lastUpdated="l"))
+        
+    #w.update()
+    
+    
+    sys.exit(app.exec_())
--- PyCSsulaBug/PyCSsulaBug/pycssulabug.py
+++ PyCSsulaBug/PyCSsulaBug/pycssulabug.py
@@ -3,56 +3,23 @@
 from PySide import QtCore, QtGui
 import views
 import handlers
+import models
 
 class MainWindow(QtGui.QMainWindow):
     
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self._mainView = views.MainView(self)
-
-        self._model = QtGui.QStandardItemModel(self)
-        self._proxyModel = QtGui.QSortFilterProxyModel(self)
-        self._siteHandler = handlers.SiteHandler(self)
-
-        self._model.setColumnCount(5);
-        self._model.setHorizontalHeaderItem(0, QtGui.QStandardItem("Name"));
-        self._model.setHorizontalHeaderItem(1, QtGui.QStandardItem("Site"));
-        self._model.setHorizontalHeaderItem(2, QtGui.QStandardItem("Type"));
-        self._model.setHorizontalHeaderItem(3, QtGui.QStandardItem("Author"));
-        self._model.setHorizontalHeaderItem(4, QtGui.QStandardItem("LastUpdated"));
-
-        #Filter
-        self._proxyModel.setSourceModel(self._model);
-        self._proxyModel.setDynamicSortFilter(True);
-
-        #SiteHandler
-        self._siteHandler.setUpdateHandler(handlers.SFUpdateHandler())
-        self._siteHandler.setDownloadHandler(handlers.SFDownloadHandler())
-        self._siteHandler.updateInfo.connect(self._insertOneEntry)
-
-        #初始設定
-        self.setCentralWidget(self._mainView)
-        self._mainView.ui.tableView.setModel(self._proxyModel)
+        self._model = models.SiteModel(self)
         self._setConnection()
+                
+        self.setCentralWidget(self._mainView)
+        self._mainView.ui.tableView.setModel(self._model.model())
 
-        self._siteHandler.update()
-            
+        self._model.update()
         
     def _setConnection(self):
-        pass
-
-    @QtCore.Slot(dict)
-    def _insertOneEntry(self, info):
-        entry = []
-        entry.append(QtGui.QStandardItem(info["name"]))
-        entry.append(QtGui.QStandardItem(info["site"]))
-        entry.append(QtGui.QStandardItem(info["type"]))
-        entry.append(QtGui.QStandardItem(info["author"]))
-        entry.append(QtGui.QStandardItem(info["lastUpdated"]))
-
-        self._model.appendRow(entry)
-
-
+        pass    
 
 if __name__ in "__main__":
     import sys
