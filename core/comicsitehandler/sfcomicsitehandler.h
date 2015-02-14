5429729b0babd2871d9974d82ce4f2b2a3c8842c
marco79423 <marco79423@gmail.com>
2015-02-14 15:09:18 +0800

    補上漏掉的內容

---
+++ core/comicsitehandler/sfcomicsitehandler.h
@@ -0,0 +1,37 @@
+﻿#ifndef SFCOMICSITESERVICE_H
+#define SFCOMICSITESERVICE_H
+
+#include "acomicsitehandler.h"
+#include "../convertz.h"
+
+class QNetworkReply;
+
+class NetworkAccessor;
+
+class SFComicSiteHandler : public AComicSiteHandler
+{
+    Q_OBJECT
+
+public:
+
+    explicit SFComicSiteHandler(QObject *parent = 0);
+    QString getComicSiteName() const;
+
+    QList<StringPair> getChapters(const QString &comicKey);
+    QStringList getImageUrls(const QString &comicKey, const QString &chapterKey);
+
+public slots:
+
+    void collectComicInfos();
+
+private slots:
+
+    void _onAccessorReply(QNetworkReply *reply);
+
+private:
+
+    NetworkAccessor *_networkAccessor;
+    ConvertZ _convertz;
+};
+
+#endif // SFCOMICSITESERVICE_H
